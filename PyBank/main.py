86cac0a2480499317c7c27de35a5a1b14277cbad
chilli <70721729+hema2575@users.noreply.github.com>
2020-10-02 13:39:01 -0500

    updated the main files.

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,34 +3,37 @@ total = 0
 d = []
 pl = []
 chng=[]
-
-#calculating count, total. Populating the list named chng[]
-import os
+# Calculating total months, net total. 
 import csv
-import itertools
 with open(r"C:\Users\muthukumar\Desktop\02_SaveNWBCSHere\03_Activities&HW\Week3\python-challenge\PyBank\Resources\budget_data.csv", mode='r') as csv_file:
     inptreader = csv.reader(csv_file)
     header = next(inptreader)   
-    for row in inptreader: 
+    for row in inptreader:
+        count = count + 1
+        total = total + int(row[1])
+#Populating the list named chng[], calculating average difference in profit/losses       
         d.append(row[0])
         pl.append(int(row[1]))
 chng = [y-x for x, y in zip(pl, pl[1:])]
-
 mx = max(chng) 
 mn = min(chng)
+avgCh = (round(sum(chng)/len(chng), 2))
+#determine the month where Greatest inc, dec in profits-diff occurs
 mxindex = chng.index(mx)
 mnindex = chng.index(mn)
 dtemx = d[mxindex+1]
 dtemn = d[mnindex+1]
+       
 
 #create the results
-with open(r"C:\Users\muthukumar\Desktop\02_SaveNWBCSHere\03_Activities&HW\Week3\python-challenge\PyBank\Resources\budget_data_1.csv", mode='r') as csv_file:
-    resultreader = csv.reader(csv_file)
-    header = next(resultreader)   
-    for row in resultreader: 
-        count = count + 1
-        total = total + int(row[1])
-        avgCh = (round(sum(chng)/len(chng), 2))
+print("\n")
+print("Financial Analysis")
+print("---------------------------------")
+print("Total Months: " + str(count))
+print("Total: " + "$" + str(total))          
+print("Average Change: " + "$" + str(avgCh))   
+print("Greatest Increase in Profits: " + dtemx + "($"+str(mx)+")")
+print("Greatest Decrease in Profits: " + dtemn + "($"+str(mn)+")")
      
 # writing the output to a text file inside Analysis folder. 
 f1 = open(r'C:\Users\muthukumar\Desktop\02_SaveNWBCSHere\03_Activities&HW\Week3\python-challenge\PyBank\Analysis\outfile.txt','w')
@@ -42,20 +45,6 @@ print("Total: " + "$" + str(total), file=f1)
 print("Average Change: " + "$" + str(avgCh), file=f1)   
 print("Greatest Increase in Profits: " + dtemx + "($"+str(mx)+")", file=f1)
 print("Greatest Decrease in Profits: " + dtemn + "($"+str(mn)+")", file=f1)
+print("\n")
 print("output to txtfile successful")
 f1.close()
-
-#print(d)
-#print(pl)
-#print(chng)
-#print(round(sum(chng)/len(chng), 2))
-#print (mx)
-#print (mn)
-#print(mxindex)
-#print(mnindex)
-#print(f" {dtemx} ($ {mx} ) ")
-#print(f" {dtemn} ($ {mn} )")
-
-
-
-
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,59 +3,34 @@ cnd = []
 cnewlist =[]
 
 #calculating count, total. Populating the list named chng[]
-import os
 import csv
 import collections
-import operator
-#from collections import Counter, OrderedDict
-#create the results
+#Find total votes cast, create list cnd[]
 with open(r"C:\Users\muthukumar\Desktop\02_SaveNWBCSHere\03_Activities&HW\Week3\python-challenge\PyPoll\Resources\election_data.csv", mode='r') as csv_file:
     pollreader = csv.reader(csv_file)
     header = next(pollreader)   
     for row in pollreader: 
         count = count + 1
         cnd.append(row[2])
-#print(count)
-
-
-#Create a key-value pair of cset, the dict. Key = cset,Value= total number of votes 
-#This is the list created out of row[2] which is candidates
-#print(cnd)
-#using set to get a dictionary of unique candidates
-cset = set(cnd)
-#print(cset) 
-#Converting the dictionary of unique candidates to a list
-cnewlist = list(cset)
-#print(cnewlist)
-# This will produce a dict that looks like Counter({'khan':2218231, ....})
-vtpercandidate = Counter(cnd)
-#print(vtpercandidate)
-#This is the summary of poll results***
+# This will produce a dict that looks like Counter({'khan':2218231, ....}). We will use this to create the summary of poll results.
+vtpercandidate = collections.Counter(cnd)
+#Determine the winner
+w = vtpercandidate.most_common(1)[0][0]
+#Create results
+print("\n")
+print("Election Results")
+print("---------------------------------")
+print("Total Votes: " + str(count))
+print("---------------------------------")    
 for x in vtpercandidate:
     print(f"{x}:  {(round(((vtpercandidate[x]/count)*100),4))}%  ({vtpercandidate[x]})")
-
-w = vtpercandidate.most_common(1)[0][0]
-print(w)
-
-#This is another way to find distinct candidates       
-#def unique(candidateslist): 
-    # intilize a null list intended to be unique list
-#    cndte = [] 
-     
-    # traverse for all elements 
-#    for x in candidateslist: 
-        # check if exists in the unique list or not 
-#        if x not in cndte:
-#            cndte.append(x)
-#    print(cndte)
-
-#unique(cnd)  
-
-
+print("---------------------------------")
+print("Winner: " + w)
+print("---------------------------------")
 
 # writing the output to a text file inside Analysis folder. 
 o = open(r'C:\Users\muthukumar\Desktop\02_SaveNWBCSHere\03_Activities&HW\Week3\python-challenge\PyPoll\Analysis\outfile','w')
-
+print("\n")
 print("Election Results", file=o)
 print("---------------------------------", file=o)
 print("Total Votes: " + str(count), file=o)
@@ -65,6 +40,7 @@ for x in vtpercandidate:
 print("---------------------------------", file=o)
 print("Winner: " + w, file=o)
 print("---------------------------------", file=o)
+print("\n")
 print("output to txtfile successful")
 o.close()
 
